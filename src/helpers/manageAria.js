f3c40bc06c7214fff042c6f0f738cf63da587bcd
Matthieu Bué <matthieu.bue@gmail.com>
2018-06-02 16:17:34 +0200

    WIP jsdoc

--- src/helpers/manageAria.js
+++ src/helpers/manageAria.js
@@ -1,6 +1,22 @@
+/**
+ * @module helpers/manageAria
+ * @desc Contains aria manager.
+ */
+
 import { CHECKED, EXPANDED, HIDDEN, SELECTED } from "../constants/constants";
 
-/* Manage ARIA attributes */
+/**
+ * Manage ARIA attributes.
+ *
+ * @param {Object} element - Current element with aria attributes to manage.
+ * @param {Json} [config={
+ * [CHECKED]: element.isToggleActive,
+ * [EXPANDED]: element.isToggleActive,
+ * [HIDDEN]: !element.isToggleActive,
+ * [SELECTED]: element.isToggleActive
+ * }] - List of aria attributes and value to test with.
+ * @returns {undefined}
+ */
 export default (
 	element,
 	config = {
