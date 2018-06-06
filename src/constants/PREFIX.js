0493ca376453a644904bfdd23cf518f2320ae607
Matthieu Bué <matthieu.bue@gmail.com>
2018-06-06 14:03:02 +0200

    Temp renaming

--- src/constants/PREFIX.js
+++
@@ -1,5 +0,0 @@
-/**
- * You can change this PREFIX value to prevent conflict with another JS library.
- * This prefix will be set to all attributes like 'data-[PREFIX]-class'.
- */
-export default "toggle";
