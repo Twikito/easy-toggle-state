c2686f4c48759f84bf90713c234310a96572b006
Matthieu Bué <matthieu.bue@gmail.com>
2018-06-12 22:14:26 +0200

    WIP

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -1,8 +1,10 @@
 (() => {
 
+	const stringify = data => data..replace(/"/g, "&​quot;").replace(/'/g, "&apos;");
+
 	const clickEventHandler = event => {
 		let JSON = {"title": "Example of tabs", "layout": "left", "js": "/* Easy Toggle State is added as external resource */", "js_external": "https://rawgit.com/Twikito/easy-toggle-state/master/dist/easy-toggle-state.es6.min.js"};
-		JSON.html = document.getElementById("tabs-demo").textContent;
+		JSON.html = stringify(document.getElementById("tabs-demo").innerHTML);
 		const response = await fetch('../assets/css/tabs.css');
 		JSON.css = await response.text();
 
