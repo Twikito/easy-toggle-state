0074bb71f924e6ac1f87756aee9f16e3012c475f
Matthieu Bué <matthieu.bue@gmail.com>
2018-06-20 14:10:11 +0200

    version update

--- dist/easy-toggle-state.js
+++ dist/easy-toggle-state.js
@@ -4,7 +4,7 @@
  * A tiny JavaScript plugin to toggle the state of any HTML element in most of contexts with ease.
  *
  * @author Matthieu Bué <https://twikito.com>
- * @version v1.5.5
+ * @version v1.6.0
  * @link https://twikito.github.io/easy-toggle-state/
  * @license MIT : https://github.com/Twikito/easy-toggle-state/blob/master/LICENSE
  * -------------------------------------------------------------------
